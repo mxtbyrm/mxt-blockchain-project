aaab2a71ba766651e6324624c9f3001ee73a5059
Sm4rtyF0x <67389748+Sm4rtyF0x@users.noreply.github.com>
2021-12-30 13:03:41 +0300

    Update create_wallet.py

--- MXT-NEW/src/create_wallet.py
+++ MXT-NEW/src/create_wallet.py
@@ -1,13 +1,38 @@
+# MIT License
+
+# Copyright (c) 2021 Sm4rtyF0x
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# Importing additional libraries
 import hashlib
 import json
 import random
 import time
 from string import ascii_letters, digits
 
+# Creating generate wallet class 
 class GenerateWallet():
     def __init__(self) -> None:
         self.time = time.time()    
     
+    # Generating wallet signature for wallet address.
     def generate_signature(self, wallet_address):
         while True:
             random_key = "".join(random.choices(ascii_letters + digits, k=100))
@@ -16,7 +41,8 @@ class GenerateWallet():
             signature = hashlib.sha256(json_signature_data.encode()).hexdigest()
             if signature.startswith("00") and signature.endswith("00"):
                 return "mxt_"+signature
-
+    
+    # Generating wallet address for new user.
     def generate_wallet(self):
         random_key = "".join(random.choices(ascii_letters + digits, k=100))
         store_wallet_data = {"local_time" : self.time, "unique_key" : random_key}
@@ -24,6 +50,7 @@ class GenerateWallet():
         wallet_address = hashlib.sha256(json_wallet_data.encode()).hexdigest()
         return "mxt_"+wallet_address
     
+    # Saving wallet data to wallets.json file
     def save_wallet(self):
         wallet_address = self.generate_wallet()
         wallet_signature = self.generate_signature(wallet_address)
@@ -37,6 +64,6 @@ class GenerateWallet():
         json_file.close()
 
         
-
+# Running generate wallet
 gw = GenerateWallet()
 gw.save_wallet()
